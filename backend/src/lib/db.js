e2581699022625e2beab33787345141d151557c9
ROMAN;) <romanavilezuw@gmail.com>
2026-05-20 22:51:15 -0700

    Update db variables to match railway

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: Number(process.env.MYSQL_PORT) || 3306
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: Number(process.env.MYSQLPORT) || 3306
 });
 
 export const ConnectDB = async () => {
